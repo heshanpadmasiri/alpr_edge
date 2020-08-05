218510a8a310882b4c82234187effcd0f7265e9a
Heshan Padmasiri <heshanpadmasiri@hotmail.com>
2020-08-05 22:09:26 +0530

    modified camera sampler to get the bounding box preditions

--- camera_scripts/camera_sampler.py
+++ camera_scripts/camera_sampler.py
@@ -1,3 +1,6 @@
+import os
+import argparse
+import tflite_runtime.interpreter as tflite
 from picamera import PiCamera
 from time import sleep
 import time
@@ -7,16 +10,34 @@ import numpy as np
 INPUT_SIZE = (480,480)
 FRAME_RATE = 10
 
-def take_image():
+def take_image(input_size):
     with picamera.PiCamera() as camera:
-        camera.resolution = INPUT_SIZE
+        camera.resolution = input_size
         camera.framerate = FRAME_RATE
         time.sleep(2)
         output = np.empty(INPUT_SIZE + (3,), dtype=np.uint8)
-        print(output.shape)
         camera.capture(output,'rgb')
     return output
 
+def run_inference(args):
+    model_path = args.model
+    interpreter = tflite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    input_shape = list(input_details[0]['shape'])
+    image_shape = (input_shape[1],input_shape[2])
+    image = take_image(image_shape) / 255.0
+    image = image.astype(np.float32).reshape(input_shape)
+    interpreter.set_tensor(input_details[0]['index'], image)
+    start = time.time()
+    interpreter.invoke()
+    print(f'execution time: {time.time() - start}')
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    print(output_data)
 
 if __name__ == '__main__':
-    print(take_image())
+    parser = argparse.ArgumentParser(description="model and input file paths")
+    parser.add_argument('model', metavar='path', type=str, help="Path to tflite model")
+    args = parser.parse_args()
+    run_inference(args)
